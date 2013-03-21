120465da9a035577770886461f956649b22a8802
Tal Yoffe <talaaronyoffe@gmail.com>
2013-03-21 12:44:43 -0400

    Test

---
+++ dbcon.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import MySQLdb
+import sys
+import os
+import platform
+
+
+
+try:
+    db = MySQLdb.connect(host="ushslabesxi-db01", user="radmin", passwd="kitkat", db="Contribs")
+
+except Exception as e:
+    sys.exit('error connecting to database')
+           
+cursor = db.cursor()
+cursor.execute('Select DISTINCT processName as procName from testSumStats')
+result = cursor.fetchall()
+print result
+
+
+    
