4ac0dd7866177073016c6e7d069c5bd84d6fe5ae
TYoffe_Fedora <TalAaronYoffe@gmail.com>
2013-03-25 12:56:20 -0400

    logging example

--- MySQL/world_select.py
+++ MySQL/world_select.py
@@ -5,18 +5,23 @@ import MySQLdb
 
 logger = logging.getLogger("DB connection")
 logger.setLevel(logging.DEBUG)
+
 #create console handler and set level to error
 ch = logging.StreamHandler()
 ch.setLevel(logging.ERROR)
+
 #create file handler and set level to debug
 fh = logging.FileHandler("dbcon.log")
 fh.setLevel(logging.DEBUG)
+
 #create formatter
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 #add formatter to ch and fh
+
 ch.setFormatter(formatter)
 fh.setFormatter(formatter)
 #add ch and fh to logger
+
 logger.addHandler(ch)
 logger.addHandler(fh)
 
